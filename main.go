79aef792d92e096838b38becfa5d304de75b2f5d
Grishy <mailgrishy@gmail.com>
2019-05-05 09:52:58 -0700

    Add dep

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println(132)
+	r := gin.Default()
+
+	// if Allow DirectoryIndex
+	//r.Use(static.Serve("/", static.LocalFile("/tmp", true)))
+	// set prefix
+	//r.Use(static.Serve("/static", static.LocalFile("/tmp", true)))
+
+	r.Use(static.Serve("/", static.LocalFile("/tmp", false)))
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "test")
+	})
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
 }
